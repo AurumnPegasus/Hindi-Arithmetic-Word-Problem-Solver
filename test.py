b56bccfcb8cfa9dd35068778de90327b815732f6
Shivansh Subramanian <54315149+AurumnPegasus@users.noreply.github.com>
2020-04-24 16:02:44 +0530

    update

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ from isc_parser import Parser
 import math
 from kartakaram import kartafunc
 from verbcategoriser import categorise
+from calculate import eq_build, c_values
 
 tk = Tokenizer(lang='hin', split_sen=True)                                  # ensures the question is split according to sentences
 tagger = Tagger(lang='hin')
@@ -34,155 +35,12 @@ what changes you need to make:
 5) if it is not a transfer, let the recepeint be the string 'none' when testing. Not that it matters but standardized
 Happy coding :) 
 '''
-c_values = [[]]
-counter_eq = 0
 
-def eq_build(karta, verb_type, value, karma, karta_number, recepient):
-    counter = 0
-    if karta_number == 1 :                                          # container [ ram ]
-        if verb_type == '0' :                               
-            if counter == 0:
-                c_values.append([int(value), karma])                # value, object         c_values = [ [4, neeli ball ] ]
-                counter += 1                                        # decleration is over
-            else:
-                ispresent = False           
-                location = len(c_values) -1                         # location is 0
-                for c_value in c_values:        
-                    if karma in c_value[1]:                         # karma is object, therefore m searching for if predeclared object is present in the sentence
-                        ispresent = True                            # object is repeated
-                        location -= 1                               # location is -1
-                        break
-                if ispresent == True:
-                    c_values[location][0] = int(value)              # so...m assigning particular new value to that object
-                else:
-                    c_values.append([int(value), karma])            # m creating another entry into c_values  c_values = [ [4, neeli ball], [6, hari ball] ]
-        if verb_type == 't+' :
-            if counter == 0:
-                c_values.append([int(value), karma])
-                c_values.append([-1*int(value), recepient])
-                counter += 1
-            else:
-                ispresent_1 = False
-                ispresent_2 = False
-                location_1 = len(c_values) -1
-                location_2 = len(c_values) -1
-                for c_value in c_values:
-                    if karma in c_value[1]:
-                        ispresent_1 = True
-                        location_1 -= 1
-                        break
-                for c_value in c_values:
-                    if recepient in c_value[1]:
-                        ispresent_2 = True
-                        location_2 -= 1
-                        break
-                if ispresent_1 == True and ispresent_2 == True:
-                    v1 = c_values[location_1][0]
-                    v2 = c_values[location_2][0]
-                    c_values[location_1][0] = v1 + int(value)
-                    c_values[location_2][0] = v2 - int(value)
-                elif ispresent_1 == True and ispresent_2 == False:
-                    v1 = c_values[location_1][0]
-                    c_values[location_1][0] = v1 + int(value)
-                    c_values.append([-1*int(value), recepient])
-                elif ispresent_1 == False and ispresent_2 == True:
-                    v2 = c_values[location_2][0]
-                    c_values[location_2][0] = v2 - int(value)
-                    c_values.append([int(value), recepient])
-                elif ispresent_1 == False and ispresent_2 == False:    
-                    c_values.append([int(value), karma])
-                    c_values.append([-1*int(value), recepient])
-        if verb_type == 't-' :                                      # verb is di, container = [ Ram, Harry ] 
-            if counter == 0:
-                c_values.append([-1*int(value), karma])             # c_values = [ [4, neeli ball], [6, hari ball], [-3, neeli ball]]
-                c_values.append([int(value), recepient])            # c_values = [ [4, neeli ball], [6, hari ball], [-3, neeli ball], [3, Harry]]
-                counter += 1
-            else:
-                ispresent_1 = False
-                ispresent_2 = False
-                location_1 = len(c_values) -1
-                location_2 = len(c_values) -1                       # both are equal to 3
-                for c_value in c_values:
-                    if karma in c_value[1]:                         # going through all objects where karma = neeli ball
-                        ispresent_1 = True
-                        location_1 -= 1                             
-                        break
-                for c_value in c_values:
-                    if recepient in c_value[1]:                     # if Harry is present, which is
-                        ispresent_2 = True
-                        location_2 -= 1
-                        break
-                if ispresent_1 == True and ispresent_2 == True:
-                    v1 = c_values[location_1][0]    
-                    v2 = c_values[location_2][0]
-                    c_values[location_1][0] = v1 - int(value)       # should not be negative, since I have already stored negative of that value
-                    c_values[location_2][0] = v2 + int(value)       # need not be added, since I have already declared Harry to be 3
-                elif ispresent_1 == True and ispresent_2 == False:
-                    v1 = c_values[location_1][0]
-                    c_values[location_1][0] = v1 - int(value)
-                    c_values.append([-1*int(value), recepient])
-                elif ispresent_1 == False and ispresent_2 == True:
-                    v2 = c_values[location_2][0]
-                    c_values[location_2][0] = v2 + int(value)
-                    c_values.append([int(value), recepient])
-                elif ispresent_1 == False and ispresent_2 == False:    
-                    c_values.append([-1*int(value), karma])
-                    c_values.append([int(value), recepient])
-        if verb_type == '++' :
-            if counter == 0:
-                c_values.append([int(value), karma])
-                counter += 1
-            else:
-                ispresent = False
-                location = len(c_values) -1
-                for c_value in c_values:
-                    if karma in c_value[1]:
-                        ispresent = True
-                        location -= 1
-                        break
-                if ispresent == True:
-                    existing_value = c_values[location][0]
-                    c_values[location][0] = existing_value + int(value)
-                else:
-                    c_values.append([int(value), karma])
-        if verb_type == '-' :
-            if counter == 0:
-                c_values.append([int(value), karma])
-                counter += 1
-            else:
-                ispresent = False
-                location = len(c_values) -1
-                for c_value in c_values:
-                    if karma in c_value[1]:
-                        ispresent = True
-                        location -= 1
-                        break
-                if ispresent == True:
-                    existing_value = c_values[location][0]
-                    c_values[location][0] = existing_value - int(value)
-                else:
-                    c_values.append(-1*[int(value), karma])
-        if verb_type == '--' :
-            if counter == 0:
-                c_values.append([int(value), karma])
-                counter += 1
-            else:
-                ispresent = False
-                location = len(c_values) -1
-                for c_value in c_values:
-                    if karma in c_value[1]:
-                        ispresent = True
-                        location -= 1
-                        break
-                if ispresent == True:
-                    existing_value = c_values[location][0]
-                    c_values[location][0] = existing_value - int(value)
-                else:
-                    c_values.append(-1*[int(value), karma])
+counter_eq = 0
         
 y = []
 y.append(sentences[37])
-print(source[37][1])
+#print(source[37][1])
 
 for i in y:
     sep_sentence = tk.tokenize(i)                                           #  Stores the list of seperated sentences within a question
@@ -198,8 +56,7 @@ for i in y:
     objects = []                                                            # Common Nouns
     assign = []																# Container, Value, Adjective, Object
     is_transfer = False
-    if( sep_sentence[len(sep_sentence)-1]):
-        print(tag_sep_sent[len(tag_sep_sent)-1])
+
 
     for j in range(0, len(sep_sentence)):
         current_sent = sep_sentence[j]
@@ -242,6 +99,7 @@ for i in y:
                 is_number = False
             elif(current_tag == 'VM'):
                 verb_type  = categorise(current_word)
+                print(verb_type)
                 if(verb_type != '0' and is_transfer == False):
                     is_transfer = True
                 
