7b1d043af73e0db8fd77e3d63db5f60e49f25d7c
Shivansh Subramanian <54315149+AurumnPegasus@users.noreply.github.com>
2020-04-24 14:00:12 +0530

    update

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ from isc_tokenizer import Tokenizer
 from isc_tagger import Tagger
 from isc_parser import Parser
 import math
+from kartakaram import kartafunc
+from verbcategoriser import categorise
 
 tk = Tokenizer(lang='hin', split_sen=True)                                  # ensures the question is split according to sentences
 tagger = Tagger(lang='hin')
@@ -179,66 +181,76 @@ def eq_build(karta, verb_type, value, karma, karta_number, recepient):
                     c_values.append(-1*[int(value), karma])
         
 y = []
-y.append(sentences[17])
+y.append(sentences[37])
+print(source[37][1])
 
 for i in y:
-	sep_sentence = tk.tokenize(i)                                           #  Stores the list of seperated sentences within a question
-	counter += 1
-	if(counter == 2):
-		break
-	tag_sep_sent = []                                                       # Stores the corresponding tags
-	for j in sep_sentence:
-		tag_sep_sent.append(tagger.tag(j))
-	print(tag_sep_sent)
-	containers = []                                                         # Proper Nouns
-	values = []                                                             # Numbers
-	objects = []                                                            # Common Nouns
-	assign = []																# Container, Value, Adjective, Object
-	for j in range(0, len(sep_sentence)):
-		current_sent = sep_sentence[j]
-		current_tagset = tag_sep_sent[j]
-		is_number = False
-		store_adj = ""                                                      # Stores the adjective assosciated with a object
-		store_last_proper = ""                                              # Stores the required proper noun, which is the first to the right of QC
-		for k in range(0, len(current_tagset)-1):   
-			current_word = current_sent[k]
-			current_tag = current_tagset[k][1]
-			next_word = current_sent[k+1]
-			next_tag = current_tagset[k+1][1]
-			if(current_word[0].isnumeric()):
-				current_tag = 'QC'
-			if( next_word[0].isnumeric()):
-				next_tag = 'QC'
-			if(current_tag == 'PRP' and len(assign) != 0):
-				current_word = assign[len(assign)-1][0]
-			
-			if(current_word == "$"):                                        # Handles special case: Money
-				objects.append("$")
-				continue
-			if(current_tag == 'JJ' and (next_tag == 'NN' or next_tag == 'NNS' ) and is_number):
-				store_adj = current_word
-				continue
-			if(current_tag == 'QC'):                    
-				if( current_word[0].isnumeric):                      
-					values.append(current_word)
-					is_number = True
-					containers.append(store_last_proper)
-			elif(current_tag == 'NNP' or current_tag == 'NNPC' or current_tag == 'PRP'):
-				store_last_proper = current_word                           # stores the last proper noun before the quantifier
-			elif((current_tag == 'NN' or current_tag == 'NNS') and is_number):
-				concat_string = store_adj + " " + current_word
-				objects.append(concat_string)
-				r = [store_last_proper, values[len(values)-1], store_adj, current_word]
-				assign.append(r)
-				store_adj = ""
-				is_number = False
-			if(current_tag == 'VM' or current_tag == 'VAUX'):
-				verb_type = 0
+    sep_sentence = tk.tokenize(i)                                           #  Stores the list of seperated sentences within a question
+    counter += 1
+    if(counter == 2):
+        break
+    tag_sep_sent = []                                                       # Stores the corresponding tags
+    for j in sep_sentence:
+        tag_sep_sent.append(tagger.tag(j))
+    print(tag_sep_sent)
+    containers = []                                                         # Proper Nouns
+    values = []                                                             # Numbers
+    objects = []                                                            # Common Nouns
+    assign = []																# Container, Value, Adjective, Object
+    is_transfer = False
+    if( sep_sentence[len(sep_sentence)-1]):
+        print(tag_sep_sent[len(tag_sep_sent)-1])
+
+    for j in range(0, len(sep_sentence)):
+        current_sent = sep_sentence[j]
+        current_tagset = tag_sep_sent[j]
+        is_number = False
+        store_adj = ""                                                      # Stores the adjective assosciated with a object
+        store_last_proper = ""                                              # Stores the required proper noun, which is the first to the right of QC
+        verb_list = []
+        for k in range(0, len(current_tagset)-1):   
+            current_word = current_sent[k]
+            current_tag = current_tagset[k][1]
+            next_word = current_sent[k+1]
+            next_tag = current_tagset[k+1][1]
+            if(current_word[0].isnumeric()):
+                current_tag = 'QC'
+            if( next_word[0].isnumeric()):
+                next_tag = 'QC'
+            if(current_tag == 'PRP' and len(assign) != 0):
+                current_word = assign[len(assign)-1][0]
+            
+            if(current_word == "$"):                                        # Handles special case: Money
+                objects.append("$")
+                continue
+            if(current_tag == 'JJ' and (next_tag == 'NN' or next_tag == 'NNS' ) and is_number):
+                store_adj = current_word
+                continue
+            if(current_tag == 'QC'):                    
+                if( current_word[0].isnumeric()):                      
+                    values.append(current_word)
+                    is_number = True
+                    containers.append(store_last_proper)
+            elif(current_tag == 'NNP' or current_tag == 'NNPC' or current_tag == 'PRP'):
+                store_last_proper = current_word                           # stores the last proper noun before the quantifier
+            elif((current_tag == 'NN' or current_tag == 'NNS') and is_number):
+                concat_string = store_adj + " " + current_word
+                objects.append(concat_string)
+                r = [store_last_proper, values[len(values)-1], concat_string]
+                assign.append(r)
+                store_adj = ""
+                is_number = False
+            elif(current_tag == 'VM'):
+                verb_type  = categorise(current_word)
+                if(verb_type != '0' and is_transfer == False):
+                    is_transfer = True
+                
+
+    print(i)
+    print(containers)
+    print(values)
+    print(objects)
+    print(assign)
 
 
-	print(i)
-	print(containers)
-	print(values)
-	print(objects)
-	print(assign)
 
